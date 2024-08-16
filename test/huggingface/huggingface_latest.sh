899e51cf1b520c03b418948a43ff388e9342aeb3
nkkko <nikola.balic@gmail.com>
2024-08-16 16:51:37 +0200

    fix hgfc latest

--- test/huggingface/huggingface_latest.sh
+++ test/huggingface/huggingface_latest.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# This test file will be executed against an auto-generated devcontainer.json that
-# includes the 'huggingface' feature with no options.
+# This test file will be executed against the 'huggingface_latest' scenario.
 
 set -e
 
@@ -20,6 +19,9 @@ check "datasets installed" python -c "import datasets; print(datasets.__version_
 check "tokenizers installed" python -c "import tokenizers; print(tokenizers.__version__)"
 check "sentencepiece installed" python -c "import sentencepiece; print(sentencepiece.__version__)"
 check "huggingface_hub installed" python -c "import huggingface_hub; print(huggingface_hub.__version__)"
+
+# Check if the installed version is the latest
+check "transformers is latest" [ "$(pip list | grep transformers | awk '{print $2}')" = "$(pip install transformers== 2>&1 | grep -oP '(?<=\(from versions: ).*(?=\))' | tr ',' '\n' | sort -V | tail -n 1)" ]
 
 # Report result
 reportResults
